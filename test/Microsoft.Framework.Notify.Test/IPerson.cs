cbef14e043656da7e4f5a51fd297a7e3bf03765b
Louis DeJardin <lodejard@microsoft.com>
2015-04-20 12:16:14 -0700

    "Working" a bit

--- test/Microsoft.Framework.Notify.Test/IPerson.cs
+++ test/Microsoft.Framework.Notify.Test/IPerson.cs
@@ -17,8 +17,10 @@
     public class Person
     {
         public string FirstName { get; set; }
+        public string LastName { get; set; }
         public Address Address { get; set; }
     }
+
     public class Address
     {
         public string City { get; set; }
